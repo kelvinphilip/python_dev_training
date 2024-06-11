08b341befd8acec574d3ab4319ac4c45ccb265a9
Philip, Kelvin <Kelvin.Philip@amd.com>
2024-06-11 14:59:41 -0400

    Dataclass example

--- classes/dpmdataclass.py
+++ classes/dpmdataclass.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass, field
 from enum import Enum
+from typing import List
 
 class LinkSpeed(Enum):
     GEN1 = 1
@@ -19,49 +21,44 @@ class LCLKFrequency(Enum):
     FREQ200MHZ = '200MHz'
     FREQ300MHZ = '300MHz'
 
+# Define the DPMLevel class as a dataclass
+@dataclass
 class LinkDPM:
-    """
-    LinkDPM class to apply DPM settings and check link state
-    """
-    dpmlevels = 0   # Class variable to keep track of DPM levels
+    speed: LinkSpeed
+    width: LinkWidth
+    lclkfreq: LCLKFrequency
+    speedhistory: List[LinkSpeed] = field(default_factory=list, repr=False)
+    debug: bool = field(default=False, repr=False)
 
-    def __init__(self, speed:LinkSpeed, width:LinkWidth, lclkfreq:LCLKFrequency) -> None:
-        self.debug = False
-        self.speed = speed.name
-        self.width = width.name
-        self.lclkfreq = lclkfreq.value
-        self.speedhistory = []
-        LinkDPM.dpmlevels += 1
-    
-    def __str__(self) -> str:
-        return f'DPM Settings :: Speed={self.speed} :: Width={self.width} :: LCLK Frequency={self.lclkfreq}'
+    def __post_init__(self):
+        self.speedhistory.append(LinkSpeed.GEN1)
 
     def apply_dpm(self):
-        print(f'Applying DPM settings :: Speed={self.speed} :: Width={self.width}')
+        print(f'Applying DPM settings :: Speed={self.speed.name} :: Width={self.width.name}')
         self.set_link_speed(self.speed)
         self.set_link_width(self.width)
-        print(f'Switch LCLK Frequency: {self.lclkfreq}')
+        print(f'Switch LCLK Frequency: {self.lclkfreq.value}')
     
     def check_link_state(self):
         print('Checking link state...')
         currentspeed = self.get_link_speed()
-        speedstatus = self.speed == currentspeed
+        speedstatus = self.speed.name == currentspeed
         if not speedstatus:
-            print(f'Link is DOWN :: Speed Mismatch :: Expected {self.speed} but got {currentspeed}')
+            print(f'Link is DOWN :: Speed Mismatch :: Expected {self.speed.name} but got {currentspeed}')
         currentwidth = self.get_link_width()
-        widthstatus = self.width == currentwidth
+        widthstatus = self.width.name == currentwidth
         if not widthstatus:
-            print(f'Link is DOWN :: Width Mismatch :: Expected {self.width} but got {currentwidth}')
+            print(f'Link is DOWN :: Width Mismatch :: Expected {self.width.name} but got {currentwidth}')
 
     def get_speed_history(self):
         print(self.speedhistory)
-    
-    def set_link_speed(self, speed):
+
+    def set_link_speed(self, speed: LinkSpeed):
         self.speedhistory.append(speed)
-        print(f'Setting Link Speed to {speed}')
+        print(f'Setting Link Speed to {speed.name}')
     
-    def set_link_width(self, width):
-        print(f'Setting Link Width to {width}')
+    def set_link_width(self, width:LinkWidth):
+        print(f'Setting Link Width to {width.name}')
 
     def get_link_speed(self):
         currentlinkspeed = 'GEN2'
@@ -70,22 +67,19 @@ class LinkDPM:
     def get_link_width(self):
         currentlinkwidth = 'x4'
         return currentlinkwidth
-        
+    
 # Define the LinkDPM instances
 DPM0 = LinkDPM(speed=LinkSpeed.GEN1, width=LinkWidth.x1, lclkfreq=LCLKFrequency.FREQ100MHZ)
 print(DPM0)
-print(f'Total DPM levels: {LinkDPM.dpmlevels}')
 
 DPM1 = LinkDPM(speed=LinkSpeed.GEN2, width=LinkWidth.x4, lclkfreq=LCLKFrequency.FREQ200MHZ)
 print(DPM1)
-print(f'Total DPM levels: {LinkDPM.dpmlevels}')
 
 DPM2 = LinkDPM(speed=LinkSpeed.GEN5, width=LinkWidth.x16, lclkfreq=LCLKFrequency.FREQ300MHZ)
 print(DPM2)
-print(f'Total DPM levels: {LinkDPM.dpmlevels}')
 
 # Apply DPM settings and check link state
 DPM0.apply_dpm()
 DPM0.check_link_state()
 DPM2.apply_dpm()
-DPM2.get_speed_history() 
+DPM2.get_speed_history()
